0ca3534c7f06beed436f049a3e9b4c09782d4d39
Renan Santana <renan_ota@hotmail.com>
2022-02-24 00:58:19 -0300

    feat: add djangorestframework-simplejwt

--- backend/django_api/django_api/settings.py
+++ backend/django_api/django_api/settings.py
@@ -36,6 +36,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
 ROOT_URLCONF = "django_api.urls"
 
 TEMPLATES = [
