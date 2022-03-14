c645bbd0e2fbbc458d588cd9102d3d5efc0b27cd
Renan Santana <renan_ota@hotmail.com>
2022-03-14 19:35:29 -0300

    refactor(adin): better admin sites

--- backend/django_api/cinema/admin.py
+++ backend/django_api/cinema/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from cinema import models
+from core.admin import BaseAdmin
 
 
-admin.site.register(models.Cinema)
-admin.site.register(models.Room)
+@admin.register(models.Cinema)
+class CinemaAdmin(BaseAdmin):
+    list_display = ("name", "address")
+
+
+@admin.register(models.Room)
+class RoomAdmin(BaseAdmin):
+    list_display = ("number", "cinema", "capacity")
--- backend/django_api/core/admin.py
+++ backend/django_api/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class BaseAdmin(admin.ModelAdmin):
+    readonly_fields = ("id",)
