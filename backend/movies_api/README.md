c3287071ea07925911f5034b3be6477cd169750f
Renan Santana <renan_ota@hotmail.com>
2024-03-04 15:55:48 -0300

    Add movies API data

--- backend/movies_api/README.md
+++ backend/movies_api/README.md
@@ -1,3 +1,15 @@
 # Movies API
 
-This microservice simulates an API to give info about movies.
+This API simulates a movies API.
+
+It serves movie data in JSON format like following:
+
+```json
+[
+    {
+        "title": "The Godfather",
+        "director": "Francis Ford Coppola",
+        "synopsis": "A classic film portraying the saga of an Italian mafia family in the United States."
+    },
+]
+```
--- backend/movies_api/src/main.py
+++ backend/movies_api/src/main.py
@@ -1,5 +1,11 @@
+import os
+import json
+from time import sleep
 from fastapi import FastAPI
 
+
+# The RESPONSE_DELAY param simulates a slow API.
+RESPONSE_DELAY = int(os.getenv("RESPONSE_DELAY"))
 
 app = FastAPI()
 
@@ -7,3 +13,10 @@ app = FastAPI()
 @app.get("/ping")
 async def ping():
     return "pong!"
+
+@app.get("/movies")
+async def movies():
+    sleep(RESPONSE_DELAY)
+    with open("movies.json", "r") as file:
+        movies_data = json.load(file)
+        return movies_data
