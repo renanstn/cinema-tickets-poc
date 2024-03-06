55c350b499951da6e031b79683c05eb48ce919f8
Renan Santana <renan_ota@hotmail.com>
2024-03-06 15:44:53 -0300

    Allows to get rooms filtered by cinema ID on URL

--- backend/django_api/cinema/urls.py
+++ backend/django_api/cinema/urls.py
@@ -1,4 +1,6 @@
+from django.urls import path, include
 from rest_framework import routers
+
 from cinema.api import viewsets
 
 
@@ -7,3 +9,12 @@ router = routers.DefaultRouter()
 router.register(r"cinemas", viewsets.CinemaViewSet, basename="cinemas")
 router.register(r"rooms", viewsets.RoomViewSet, basename="rooms")
 router.register(r"ping", viewsets.PingCinemaViewSet, basename="ping")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "cinemas/<uuid:cinema_id>/rooms",
+        viewsets.RoomsList.as_view(),
+        name="list_rooms",
+    ),
+]
