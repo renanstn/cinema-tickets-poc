3b12af99fe95ff6739c442a7497dabf0e4a9d394
Renan Santana <renan_ota@hotmail.com>
2024-03-07 00:21:20 -0300

    Update hot_reload

--- tools/scripts/hot_test.sh
+++ tools/scripts/hot_test.sh
@@ -2,9 +2,11 @@
 
 # This script acts like a "hot reload" feature for developments.
 # It runs all tests when any file is changed.
+# To use this script, you must have inotify-tools installed on your machine:
+# sudo apt install inotify-tools
 
 LOOKUP_PATH="/home/renan/GitHub/cinema-tickets-poc"
 
-while inotifywait -r -e modify --exclude '\.git|\.md' $LOOKUP_PATH; do
+while inotifywait -r -e modify --exclude '\.git|\.md|\.sh' $LOOKUP_PATH; do
     docker-compose run --rm api python manage.py test;
 done
